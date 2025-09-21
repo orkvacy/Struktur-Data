3c664ea31cecba36813b145ca4cf88b2aa6fcc18
orkvacy <krotago@gmail.com>
2025-09-21 15:59:39 +0800

    feat/showtable

--- POSTTEST_SDA/POSTTEST_2/main.cpp
+++ POSTTEST_SDA/POSTTEST_2/main.cpp
@@ -12,6 +12,9 @@ struct jadwalPenerbangan {
     jadwalPenerbangan *next;
 };
 
+jadwalPenerbangan *head = nullptr;
+int pkCode = 0;
+
 string buatKodePenerbangan() {
     if (pkCode == 0) {
         pkCode++;
@@ -22,5 +25,29 @@ string buatKodePenerbangan() {
     return kode;
 }
 
-jadwalPenerbangan *head = nullptr;
-int pkCode = 0;
+void delayInput() {
+    cout << "Tekan Enter untuk kembali ke menu :D" << endl;
+    cin.ignore(numeric_limits<streamsize>::max());
+}
+
+void showJadwal(){
+    system("cls");
+    cout << "\tJadwal Penerbangan\n";
+
+    if (head == nullptr) {
+        cout << "Belum ada jadwal penerbangan yang ditambahkan." << endl;
+        return;
+    }
+    Table jadwalTable;
+    jadwalTable.add_row({"No.", "Kode Penerbangan", "Tujuan", "Status"});
+
+    jadwalPenerbangan *temp = head;
+    int no = 1;
+    while (temp != nullptr) {
+        jadwalTable.add_row({to_string(no++), temp->kodePenerbangan, temp->tujuan, temp->status});
+        temp = temp->next;
+    }
+
+    cout << jadwalTable << endl;
+
+}
